919c88c6db0700d056bc3e93fef90dffb2aef4ae
anthonym01 <samuelmatheson15@gmail.com>
2025-10-06 17:48:24 -0500

    Update LAS_track.js to correct Traccar API endpoint and enhance GPS data handling from Land Air Sea response

--- tests/LAS_track.js
+++ tests/LAS_track.js
@@ -15,7 +15,7 @@ async function track() {
 
             // 2. Send the request with the form data
             const response = await axios.post(
-                config.traccar.url + '/api/session',
+                config.traccar.url_webui + '/api/session',
                 params.toString() // Pass the URL-encoded string
             );
 
@@ -33,34 +33,67 @@ async function track() {
     axios.post(
         'https://gateway.landairsea.com/Track/MyDevices',
         config.LandAirSea,
-        //{ headers: { 'ClientId': config.LandAirSea.ClientId } }
+        { headers: { 'ClientId': config.LandAirSea.clientToken } }
     ).then(responseLAS => {//pass data to traccar
         // Handle the successful response
         console.log('LAND AIR SEA API Response:', responseLAS.data);
+        /* Example data from LAS
+            username: 'something',
+            devicedetails: [
+                {
+              deviceId: '0002060709',
+              name: 'BMW 116i',
+              notes: '',
+              latitude: 0.4567725832264,
+              longitude: -0.8930006667972,
+              lastlocation: 'Da ocean',
+              speed_kmh: 0,
+              heading: 93,
+              elevation: 279,
+              group: 'Tracking',
+              battery: 100,
+              voltage: -1,
+              isstopped: false,
+              is_cell_assist: false,
+              is_wired: false,
+              is_led_on: true,
+              has_tamper: false,
+              cellularstrength: 15,
+              satellitestrength: 9,
+              temperature: null,
+              interval: 180,
+              lftminutes: 0,
+              icon: 'https://portal.gps-tracking.com/imagery/mapicons/red-car/red-car-90.png',
+              vin: '',
+              usertime: '10/05/2025 11:01:08 PM',
+              utctime: '10/06/2025 4:01:08 AM'
+            }
+        ]
+            */
 
         axios.defaults.headers.common['Authorization'] = 'Bearer ' + traccarSession.token;//set traccar cookie
 
         console.log('Posting to Traccar for device ID:', responseLAS.data.devicedetails[1].deviceId);
 
-        const traccar_protocol_port = 5055;
+        //const traccar_protocol_port = 5055;
 
         // Replace with your actual server URL, or use config.traccar.url if it includes the port
-        const server_base_url = config.traccar.url.replace(/:\d+/, `:${traccar_protocol_port}`);
+        //const server_base_url = config.traccar.url.replace(/:\d+/, `:${traccar_protocol_port}`);
 
         // translate gps data to traccar format
         const gpsData = {
             id: Number(responseLAS.data.devicedetails[1].deviceId), // The unique ID you set for the device
-            lat: 34.0522,
-            lon: -118.2437,
+            lat:responseLAS.data.devicedetails[1].latitude,
+            lon: responseLAS.data.devicedetails[1].longitude,
             timestamp: Date.now(), // Current time in milliseconds
-            speed: 20, // in knots
-            altitude: 100, // in meters
+            speed: responseLAS.data.devicedetails[1].speed_kmh * 0.539957, // in knots
+            altitude: responseLAS.data.devicedetails[1].elevation, // in meters
             valid: true,
-            batt: 95
+            batt: responseLAS.data.devicedetails[1].battery // battery level percentage
         };
 
         axios.get(
-            `${server_base_url}/`, // The path is just the root '/'
+            `${config.traccar.url_OsmAndport}/`, // The path is just the root '/'
             {
                 params: gpsData // Axios will automatically convert this object to a query string
             }
