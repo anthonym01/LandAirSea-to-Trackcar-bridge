4519735053cc7d286a832d8fb30636e79e1a5a7c
anthonym01 <samuelmatheson15@gmail.com>
2025-10-08 23:12:56 -0500

    Refactor LAS_traccar_server_bridge_driver.js for improved logging and session management; remove database module; update package.json and test scripts for consistency

--- LAS_traccar_server_bridge_driver.js
+++ LAS_traccar_server_bridge_driver.js
@@ -4,22 +4,28 @@
 //requires devices to be preconfigured in Traccar with matching device IDs
 
 //Server configuration
-const port = 8082;// 80, 443, 8082 nginx
+const port = 8083;// 80, 443, 8083 nginx
 const express = require('express');
 const app = express();
 const logs = require('./modules/logger');
 const axios = require('axios');
 const fs = require('fs');
-let credentials = JSON.parse(fs.readFileSync('configuration.json'));
+let config = JSON.parse(fs.readFileSync('configuration.json'));
+let updateinterval = null;
+let traccarSession = null;
 
 app.listen(port, () => {
     try {
         logs.initalize();//initalize logger
         logs.info('Server starting');//log server start
-        logs.info('Running on port ', port);
+        setTimeout(() => {
+            logs.info('Running on port ', port);
         logs.info('Process ID: ', process.pid);
         logs.info('Process path: ', process.cwd());
-        //database.initalize();//initalize database
+        }, 3000);//wait 3 seconds for splash
+        
+        track();
+        updateinterval = setInterval(() => { track() }, 60000);
     } catch (error) {
         logs.error('Catastrophy on server start: ', error);
     }
@@ -27,30 +33,105 @@ app.listen(port, () => {
 
 //bind root path to /www folder
 app.use(express.static('www')).listen(() => { logs.info('Static files served from ', process.cwd(), '/www'); });
+app.use(express.json());//for parsing application/json
+app.use(express.urlencoded({ extended: true }));//for parsing application/x-www-form-urlencoded
 
-app.get('/get/test', (req, res) => {//test get
-    try {
-        logs.info('test get');
-        req.on('data', function (data) {
-            logs.info('got: ', data);
-            res.end(JSON.stringify({ testget: "test get data received" }));
-        });
-        //res.writeHead(200, { 'Content-type': 'application/json' });
-        res.send(JSON.stringify({ test: 'test get is okay' }));
-    } catch (error) {
-        logs.error('Catastrophy on test get: ', err);
-    }
-});
+async function track() {//main tracking function prototype
 
-app.post('/post/test', (req, res) => {//test post
-    //receive more data than a get
-    try {
-        logs.info('test post to server');
-        req.on('data', function (data) {
-            logs.info('Posted : ', JSON.parse(data));
-            res.end(JSON.stringify({ test: "test post received" }));
-        });
-    } catch (error) {
-        logs.error('Catastrophy on test post: ', err);
+    //login to traccar if no session
+    if (!traccarSession || traccarSession == null) {
+        try {
+            logs.info(`Attempt Traccar login to ${config.traccar.url_webui} @ ${new Date().toISOString()} with user ${config.traccar.username}`);
+
+            const params = new URLSearchParams();
+            params.append('email', config.traccar.username);
+            params.append('password', config.traccar.password);
+
+            const response = await axios.post(
+                config.traccar.url_webui + '/api/session',
+                params.toString() // Pass the URL-encoded string
+            );
+
+            traccarSession = response.data;
+            logs.info('Traccar login successful:', traccarSession);
+        } catch (error) {
+            logs.error('Traccar login error:', error.message);
+            logs.info('Check Traccar URL, username, and password in configuration.json, remember to include http or https in the URL along with any port number if needed.');
+            return; // Exit the function if login fails
+        }
     }
-});
+
+    //reset axios headers to default
+    axios.defaults.headers.common = {};//set traccar cookie later
+
+    //Pull post from land air sea
+    logs.info(`Pulling data from LandAirSea @ ${new Date()}`);
+    axios.post(
+        'https://gateway.landairsea.com/Track/MyDevices',
+        config.LandAirSea,
+        { headers: { 'ClientId': config.LandAirSea.clientToken } }
+    ).then(responseLAS => {//pass data to traccar
+        /* 
+        Example data from LAS:
+        {
+            username: 'something',
+            devicedetails: [
+                { deviceId, name, notes, latitude, longitude, lastlocation,            speed_kmh, heading, elevation, group, battery, voltage,              isstopped, is_cell_assist, is_wired, is_led_on, has_tamper,              cellularstrength,satellitestrength, temperature, interval,           lftminutes, icon, vin, usertime, utctime}
+            ]
+        } */
+
+        //loop through devices in response
+        responseLAS.data.devicedetails.forEach(device => {
+            logs.info(`---------------------------------------------------------------`);
+            logs.info(`@ ${new Date()}`);
+            logs.info(`Device ID: ${device.deviceId}`);
+            logs.info(`Lattitude: ${device.latitude}`);
+            logs.info(`longitude: ${device.longitude}`);
+            logs.info(`Speed (km/h): ${device.speed_kmh}`);
+            logs.info(`Battery: ${device.battery}%`);
+            logs.info(`voltage: ${device.voltage}V`);
+            logs.info(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + traccarSession.token;//set traccar cookie
+
+            let charging = false;
+            if (device.voltage != -1) { charging = true }
+
+            const gpsData = {
+                id: Number(device.deviceId), // The unique ID you set for the device
+                lat: device.latitude,
+                lon: device.longitude,
+                timestamp: Date.now(), // Current time in milliseconds
+                speed: device.speed_kmh * 0.539957, // in knots
+                altitude: device.elevation, // in meters
+                valid: true,
+                batt: device.battery, // battery level percentage
+                charge: charging // voltage level
+            };
+
+            //post data to traccar
+            axios.get(config.traccar.url_OsmAndport, { params: gpsData }).then(responsefromTraccar => {
+                logs.info(`Successfully updated GPS data for device ${gpsData.id}. Status: ${responsefromTraccar.status}`);
+
+            }).catch(error => {
+                logs.error(`Error sending GPS data: ${error.message}`);
+                //attempt to login again next time
+                traccarSession = null;
+            });
+        });
+    }).catch(error => {
+
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            logs.error('Error Response Data:', error.response.data);
+            logs.error('Error Status:', error.response.status);
+        } else if (error.request) {
+            // The request was made but no response was received
+            logs.error('No response received:', error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            logs.error('Error message:', error.message);
+        }
+    });
+}
--- tests/test_track.js
+++ tests/test_track.js
@@ -9,11 +9,12 @@ async function track() {//main tracking function prototype
     //login to traccar if no session
     if (!traccarSession || traccarSession == null) {
         try {
+            console.log(`Attempt Traccar login to ${config.traccar.url_webui} @ ${new Date().toISOString()} with user ${config.traccar.username}`);
+
             const params = new URLSearchParams();
             params.append('email', config.traccar.username);
             params.append('password', config.traccar.password);
 
-            // 2. Send the request with the form data
             const response = await axios.post(
                 config.traccar.url_webui + '/api/session',
                 params.toString() // Pass the URL-encoded string
@@ -23,6 +24,7 @@ async function track() {//main tracking function prototype
             console.log('Traccar login successful:', traccarSession);
         } catch (error) {
             console.error('Traccar login error:', error.message);
+            console.log('Check Traccar URL, username, and password in configuration.json, remember to include http or https in the URL along with any port number if needed.');
             return; // Exit the function if login fails
         }
     }
@@ -36,55 +38,31 @@ async function track() {//main tracking function prototype
         config.LandAirSea,
         { headers: { 'ClientId': config.LandAirSea.clientToken } }
     ).then(responseLAS => {//pass data to traccar
-        // Handle the successful response
-        //console.log('LAND AIR SEA API Response:', responseLAS.data);
-        /* Example data from LAS
+        /* 
+        Example data from LAS:
+        {
             username: 'something',
             devicedetails: [
-                {
-              deviceId: '0002060709',
-              name: 'BMW 116i',
-              notes: '',
-              latitude: 0.4567725832264,
-              longitude: -0.8930006667972,
-              lastlocation: 'Da ocean',
-              speed_kmh: 0,
-              heading: 93,
-              elevation: 279,
-              group: 'Tracking',
-              battery: 100,
-              voltage: -1,
-              isstopped: false,
-              is_cell_assist: false,
-              is_wired: false,
-              is_led_on: true,
-              has_tamper: false,
-              cellularstrength: 15,
-              satellitestrength: 9,
-              temperature: null,
-              interval: 180,
-              lftminutes: 0,
-              icon: 'https://portal.gps-tracking.com/imagery/mapicons/red-car/red-car-90.png',
-              vin: '',
-              usertime: '10/05/2025 11:01:08 PM',
-              utctime: '10/06/2025 4:01:08 AM'
-            }
-        ]
-            */
+                { deviceId, name, notes, latitude, longitude, lastlocation,            speed_kmh, heading, elevation, group, battery, voltage,              isstopped, is_cell_assist, is_wired, is_led_on, has_tamper,              cellularstrength,satellitestrength, temperature, interval,           lftminutes, icon, vin, usertime, utctime}
+            ]
+        } */
 
         //loop through devices in response
         responseLAS.data.devicedetails.forEach(device => {
             console.log(`@ ${new Date().toISOString()}`);
-            console.log(`Device ID: ${device.deviceId}, Lat: ${device.latitude}, Lon: ${device.longitude}, Speed (km/h): ${device.speed_kmh}, Battery: ${device.battery}%`);
+            console.log(`----------------------------------------------------`);
+            console.log(`Device ID: ${device.deviceId}`);
+            console.log(`Lattitude: ${device.latitude}`);
+            console.log(`longitude: ${device.longitude}`);
+            console.log(`Speed (km/h): ${device.speed_kmh}`);
+            console.log(`Battery: ${device.battery}%`);
+            console.log(`voltage: ${device.voltage}V`);
+
             axios.defaults.headers.common['Authorization'] = 'Bearer ' + traccarSession.token;//set traccar cookie
 
-            console.log('Posting to Traccar for device ID:', device.deviceId);
-
             let charging = false;
-            if (device.voltage != -1) {
-                console.log(`Device ${device.deviceId} voltage: ${device.voltage}V`);
-                charging = true;
-            }
+            if (device.voltage != -1) { charging = true }
+
             const gpsData = {
                 id: Number(device.deviceId), // The unique ID you set for the device
                 lat: device.latitude,
@@ -97,21 +75,13 @@ async function track() {//main tracking function prototype
                 charge: charging // voltage level
             };
 
-            axios.get(
-                `${config.traccar.url_OsmAndport}/`,
-                {
-                    params: gpsData // Axios will automatically convert this object to a query string
-                }
-            ).then(responseTraccar => {
-                console.log(`Successfully posted GPS data for device ${gpsData.id}. Status: ${responseTraccar.status}`);
+            axios.get(config.traccar.url_OsmAndport, { params: gpsData }).then(responsefromTraccar => {
+                console.log(`Successfully updated GPS data for device ${gpsData.id}. Status: ${responsefromTraccar.status}`);
 
             }).catch(error => {
                 console.error(`Error sending GPS data: ${error.message}`);
             });
-
         });
-
-
     }).catch(error => {
 
         if (error.response) {
