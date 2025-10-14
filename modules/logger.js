86559e98169a405e9789ff83e0cd2c14c86cffd9
anthonym01 <samuelmatheson15@gmail.com>
2025-10-14 17:19:22 -0500

    Refactor logger.js for improved error handling and argument processing; remove unused test.tracker.one.js and update package.json scripts

--- modules/logger.js
+++ modules/logger.js
@@ -38,72 +38,74 @@ const loggerite = {
     //log happenings
     info: async function (...args) {
         const log_properties = this.get_paths();//get log paths
-        
-        /*
-            Keep node consoles style for numbers objects and blobs
-        */
-        // Process arguments
-        const argummentlength = arguments.length;
-        if (argummentlength == 0) {
-            console.log("no arguments passed to logger")
-            return 0
-        } else if (argummentlength == 1) {
-            console.log(arguments[0]);
-            let arguments1 = arguments[0];
-            if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
-            writelog(arguments1);
-            return 1;
-        } else if (argummentlength == 2) {
-            console.log(arguments[0], arguments[1]);
-            let arguments1 = arguments[0];
-            let arguments2 = arguments[1];
-            if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
-            if (typeof arguments2 === 'object') arguments2 = `\n${JSON.stringify(arguments2, null, 2)}`;
-            writelog(`${arguments1}${arguments2}`);
-            return 2;
-        } else if (argummentlength == 3) {
-            console.log(arguments[0], arguments[1], arguments[2]);
-            let arguments1 = arguments[0];
-            let arguments2 = arguments[1];
-            let arguments3 = arguments[2];
-            if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
-            if (typeof arguments2 === 'object') arguments2 = `\n${JSON.stringify(arguments2, null, 2)}`;
-            if (typeof arguments3 === 'object') arguments3 = `\n${JSON.stringify(arguments3, null, 2)}`;
-            writelog(`${arguments1}${arguments2}${arguments3}`);
-            return 3;
-        } else if (argummentlength == 4) {
-            console.log(arguments[0], arguments[1], arguments[2], arguments[3]);
-            let arguments1 = arguments[0];
-            let arguments2 = arguments[1];
-            let arguments3 = arguments[2];
-            let arguments4 = arguments[3];
-            if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
-            if (typeof arguments2 === 'object') arguments2 = `\n${JSON.stringify(arguments2, null, 2)}`;
-            if (typeof arguments3 === 'object') arguments3 = `\n${JSON.stringify(arguments3, null, 2)}`;
-            if (typeof arguments4 === 'object') arguments4 = `\n${JSON.stringify(arguments4, null, 2)}`;
-            writelog(`${arguments1}${arguments2}${arguments3}${arguments4}`);
-            return 3;
-        } else {
-            let stripped_arguments = ``;
-            try {// to process passed arguments
-                for (let arg in arguments) {
-                    //console.log(arg)
-                    if (typeof arguments[arg] === 'object') {
-                        stripped_arguments = stripped_arguments + ` \n${JSON.stringify(arguments[arg], null, 2)}`;
-                        //process.stdout.write(arguments[arg]);
-                    } else {
-                        stripped_arguments = stripped_arguments + `${arguments[arg]} `;
+        try {
+            /*
+                Keep node consoles style for numbers objects and blobs
+            */
+            // Process arguments
+            const argummentlength = arguments.length;
+            if (argummentlength == 0) {
+                console.log("no arguments passed to logger")
+                return 0
+            } else if (argummentlength == 1) {
+                console.log(arguments[0]);
+                let arguments1 = arguments[0];
+                if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
+                writelog(arguments1);
+                return 1;
+            } else if (argummentlength == 2) {
+                console.log(arguments[0], arguments[1]);
+                let arguments1 = arguments[0];
+                let arguments2 = arguments[1];
+                if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
+                if (typeof arguments2 === 'object') arguments2 = `\n${JSON.stringify(arguments2, null, 2)}`;
+                writelog(`${arguments1}${arguments2}`);
+                return 2;
+            } else if (argummentlength == 3) {
+                console.log(arguments[0], arguments[1], arguments[2]);
+                let arguments1 = arguments[0];
+                let arguments2 = arguments[1];
+                let arguments3 = arguments[2];
+                if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
+                if (typeof arguments2 === 'object') arguments2 = `\n${JSON.stringify(arguments2, null, 2)}`;
+                if (typeof arguments3 === 'object') arguments3 = `\n${JSON.stringify(arguments3, null, 2)}`;
+                writelog(`${arguments1}${arguments2}${arguments3}`);
+                return 3;
+            } else if (argummentlength == 4) {
+                console.log(arguments[0], arguments[1], arguments[2], arguments[3]);
+                let arguments1 = arguments[0];
+                let arguments2 = arguments[1];
+                let arguments3 = arguments[2];
+                let arguments4 = arguments[3];
+                if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
+                if (typeof arguments2 === 'object') arguments2 = `\n${JSON.stringify(arguments2, null, 2)}`;
+                if (typeof arguments3 === 'object') arguments3 = `\n${JSON.stringify(arguments3, null, 2)}`;
+                if (typeof arguments4 === 'object') arguments4 = `\n${JSON.stringify(arguments4, null, 2)}`;
+                writelog(`${arguments1}${arguments2}${arguments3}${arguments4}`);
+                return 3;
+            } else {
+                let stripped_arguments = ``;
+                try {// to process passed arguments
+                    for (let arg in arguments) {
+                        //console.log(arg)
+                        if (typeof arguments[arg] === 'object') {
+                            stripped_arguments = stripped_arguments + ` \n${JSON.stringify(arguments[arg], null, 2)}`;
+                            //process.stdout.write(arguments[arg]);
+                        } else {
+                            stripped_arguments = stripped_arguments + `${arguments[arg]} `;
+                        }
                     }
+                    console.log(stripped_arguments)
+                    //process.stdout.write('\n');
+                    writelog(stripped_arguments);
+                    return arguments.length;
+                } catch (err) {
+                    console.error(err)
                 }
-                console.log(stripped_arguments)
-                //process.stdout.write('\n');
-                writelog(stripped_arguments);
-                return arguments.length;
-            } catch (err) {
-                console.error(err)
             }
+        } catch (error) {
+            console.error("Logging error", error)
         }
-
         //write to log file
         function writelog(datum) {
             try {
@@ -120,66 +122,69 @@ const loggerite = {
             Keep node consoles style for numbers objects and blobs
         */
         // Process arguments
-        const argummentlength = arguments.length;
-        if (argummentlength == 0) {
-            console.log("no arguments passed to logger")
-            return 0;
-        } else if (argummentlength == 1) {
-            console.error(arguments[0]);
-            let arguments1 = arguments[0];
-            if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
-            writelog(arguments1);
-            return 1;
-        } else if (argummentlength == 2) {
-            console.error(arguments[0], arguments[1]);
-            let arguments1 = arguments[0];
-            let arguments2 = arguments[1];
-            if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
-            if (typeof arguments2 === 'object') arguments2 = `\n${JSON.stringify(arguments2, null, 2)}`;
-            writelog(`${arguments1}${arguments2}`);
-            return 2;
-        } else if (argummentlength == 3) {
-            console.error(arguments[0], arguments[1], arguments[2]);
-            let arguments1 = arguments[0];
-            let arguments2 = arguments[1];
-            let arguments3 = arguments[2];
-            if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
-            if (typeof arguments2 === 'object') arguments2 = `\n${JSON.stringify(arguments2, null, 2)}`;
-            if (typeof arguments3 === 'object') arguments3 = `\n${JSON.stringify(arguments3, null, 2)}`;
-            writelog(`${arguments1}${arguments2}${arguments3}`);
-            return 3;
-        } else if (argummentlength == 4) {
-            console.error(arguments[0], arguments[1], arguments[2], arguments[3]);
-            let arguments1 = arguments[0];
-            let arguments2 = arguments[1];
-            let arguments3 = arguments[2];
-            let arguments4 = arguments[3];
-            if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
-            if (typeof arguments2 === 'object') arguments2 = `\n${JSON.stringify(arguments2, null, 2)}`;
-            if (typeof arguments3 === 'object') arguments3 = `\n${JSON.stringify(arguments3, null, 2)}`;
-            if (typeof arguments4 === 'object') arguments4 = `\n${JSON.stringify(arguments4, null, 2)}`;
-            writelog(`${arguments1}${arguments2}${arguments3}${arguments4}`);
-            return 3;
-        } else {
-            let stripped_arguments = ``;
-            try {// to process passed arguments
-                for (let arg in arguments) {
-                    //console.log(arg)
-                    if (typeof arguments[arg] === 'object') {
-                        stripped_arguments = stripped_arguments + ` \n${JSON.stringify(arguments[arg], null, 2)}`;
-                        //process.stdout.write(arguments[arg]);
-                    } else {
-                        stripped_arguments = stripped_arguments + `${arguments[arg]} `;
+        try {
+            const argummentlength = arguments.length;
+            if (argummentlength == 0) {
+                console.log("no arguments passed to logger")
+                return 0;
+            } else if (argummentlength == 1) {
+                console.error(arguments[0]);
+                let arguments1 = arguments[0];
+                if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
+                writelog(arguments1);
+                return 1;
+            } else if (argummentlength == 2) {
+                console.error(arguments[0], arguments[1]);
+                let arguments1 = arguments[0];
+                let arguments2 = arguments[1];
+                if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
+                if (typeof arguments2 === 'object') arguments2 = `\n${JSON.stringify(arguments2, null, 2)}`;
+                writelog(`${arguments1}${arguments2}`);
+                return 2;
+            } else if (argummentlength == 3) {
+                console.error(arguments[0], arguments[1], arguments[2]);
+                let arguments1 = arguments[0];
+                let arguments2 = arguments[1];
+                let arguments3 = arguments[2];
+                if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
+                if (typeof arguments2 === 'object') arguments2 = `\n${JSON.stringify(arguments2, null, 2)}`;
+                if (typeof arguments3 === 'object') arguments3 = `\n${JSON.stringify(arguments3, null, 2)}`;
+                writelog(`${arguments1}${arguments2}${arguments3}`);
+                return 3;
+            } else if (argummentlength == 4) {
+                console.error(arguments[0], arguments[1], arguments[2], arguments[3]);
+                let arguments1 = arguments[0];
+                let arguments2 = arguments[1];
+                let arguments3 = arguments[2];
+                let arguments4 = arguments[3];
+                if (typeof arguments1 === 'object') arguments1 = `\n${JSON.stringify(arguments1, null, 2)}`;
+                if (typeof arguments2 === 'object') arguments2 = `\n${JSON.stringify(arguments2, null, 2)}`;
+                if (typeof arguments3 === 'object') arguments3 = `\n${JSON.stringify(arguments3, null, 2)}`;
+                if (typeof arguments4 === 'object') arguments4 = `\n${JSON.stringify(arguments4, null, 2)}`;
+                writelog(`${arguments1}${arguments2}${arguments3}${arguments4}`);
+                return 3;
+            } else {
+                let stripped_arguments = ``;
+                try {// to process passed arguments
+                    for (let arg in arguments) {
+                        //console.log(arg)
+                        if (typeof arguments[arg] === 'object') {
+                            stripped_arguments = stripped_arguments + ` \n${JSON.stringify(arguments[arg], null, 2)}`;
+                            //process.stdout.write(arguments[arg]);
+                        } else {
+                            stripped_arguments = stripped_arguments + `${arguments[arg]} `;
+                        }
                     }
+                    console.error(stripped_arguments);
+                    writelog(stripped_arguments);
+                    return arguments.length;
+                } catch (err) {
+                    console.error(err)
                 }
-                console.error(stripped_arguments);
-                writelog(stripped_arguments);
-                return arguments.length;
-            } catch (err) {
-                console.error(err)
             }
+        } catch (error) {
+            console.error("Logging error", error)
         }
-
         function writelog(datum) {//write to log file
             try {
                 fs.appendFileSync(log_properties.file_path, `\n****************************************\nError:\n${log_properties.timex} :\n${datum}\n******************************************\n\n`, { encoding: 'utf8' });
